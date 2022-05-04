df071e4fbb66d2d4dd92b5ac87a6e36e56b1e392
waterifire <69126570+waterifire@users.noreply.github.com>
2022-05-04 21:42:32 +0200

    Workdle done

--- ____work/____work/settings.py
+++ ____work/____work/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'hobby',
     'test_setup',
     'dykt',
+    'workdle',
 ]
 
 MIDDLEWARE = [
---
+++ ____work/workdle/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+import random
+
+from test_setup.models import WordleSetup
+from test_setup.forms import WorkdleSetupForm
+# Create your views here.
+
+def workdle_home(request):
+    people = WordleSetup.objects.all()
+    context = {'people': people,}
+    response = render(request, 'workdle/workdle_home.html', context)
+    return response
+
+def workdle_arena(request, pk):
+    color_test = 'deepskyblue'
+    person = WordleSetup.objects.get(id=pk)
+    form = WorkdleSetupForm(instance=person)
+
+    number_options = [0, 1, 2, 3, 4]
+    number_choice = random.choice(number_options)
+    question_options = [person.questions[0], person.questions[1], person.questions[2], person.questions[3], person.questions[4]]
+    word_options = [person.q1, person.q2, person.q3, person.q4, person.q5]
+
+    word_specific = word_options[number_choice].lower()
+    word_length = int(len(word_specific))
+    question_specific = question_options[number_choice]
+
+    context = {'question_specific': question_specific, 'word_length': word_length, 'word_specific': word_specific, 'person': person, 'form': form, 'color_test': color_test}
+    response = render(request, 'workdle/workdle_arena.html', context)
+    return response
